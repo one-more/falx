9c564a9f72e1c1116065b0dce41180c5ae1ed1b3
dmitrii_nikolaev <greyjoi@yandex.ru>
2018-02-07 14:00:01 +0300

    lib build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+var path = require('path');
+
 module.exports = {
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'main.js',
+        library: 'falx',
+        libraryTarget:'umd'
+    },
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
